f7ede977ee85227cab9ad17cb95fb91feb301ba9
Carlo Dee <carlodee.official@proton.me>
2024-09-27 16:19:33 +0530

    Zenith: Moved recovery resources to vendor_boot.

--- device.mk
+++ device.mk
@@ -9,7 +9,7 @@ LOCAL_PATH := device/infinix/zenith
 
 # Virtual A/B
 ENABLE_VIRTUAL_AB := true
-$(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota/launch.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota/compression.mk)
 
 # A/B
 AB_OTA_POSTINSTALL_CONFIG += \
