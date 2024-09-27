a3d05726ca372666a387d6991eec2225bad65d90
Carlo Dee <carlodee.official@proton.me>
2024-09-27 15:54:36 +0530

    Zenith: Enabled Virtual AB support.

--- device.mk
+++ device.mk
@@ -7,6 +7,10 @@
 
 LOCAL_PATH := device/infinix/zenith
 
+# Virtual A/B
+ENABLE_VIRTUAL_AB := true
+$(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota/launch.mk)
+
 # A/B
 AB_OTA_POSTINSTALL_CONFIG += \
     RUN_POSTINSTALL_system=true \
