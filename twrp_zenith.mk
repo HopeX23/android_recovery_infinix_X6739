fa8a1ee2dadc2078ba1dc3aa8afe698259bd879e
Carlo Dee <carlodee.official@proton.me>
2024-09-27 16:22:45 +0530

    Zenith: Rework inherit properties from AOSP.
    * base.mk - As we are only building with minimal manifest, we only need most basic props we need.
    * emulated_storage.mk - Enable project quotas and casefolding for emulated storage without sdcardfs.
    * userspace_reboot.mk - Stock has this properties, so we will be inherting as well to enable userspace_reboot.

--- twrp_zenith.mk
+++ twrp_zenith.mk
@@ -7,7 +7,13 @@
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
+
+#Inherit emulated_storage properties
+$(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
+
+# Inherit userspace reboot properties
+$(call inherit-product, $(SRC_TARGET_DIR)/product/userspace_reboot.mk)
 
 # Inherit some common TWRP stuff.
 $(call inherit-product, vendor/twrp/config/common.mk)
